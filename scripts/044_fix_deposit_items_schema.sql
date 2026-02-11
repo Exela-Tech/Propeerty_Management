e0d09b1e99aaeed31700897df5e2316829489ceb
Ouma Ronald <ronald2ouma2@gmail.com>
2026-02-11 20:08:23 +0300

    fixes to the deposits

--- scripts/044_fix_deposit_items_schema.sql
+++
@@ -1,7 +0,0 @@
--- Fix the deposit_items table to properly reference tenant payments
-ALTER TABLE deposit_items
-ADD CONSTRAINT fk_deposit_items_tenant_payment
-FOREIGN KEY (payment_id) REFERENCES tenant_payments(id) ON DELETE CASCADE;
-
--- Create index for better query performance
-CREATE INDEX IF NOT EXISTS idx_deposit_items_payment_id ON deposit_items(payment_id);
